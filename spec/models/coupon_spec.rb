315cca85ce99615c85c958e597bfd75979af7383
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-07 16:24:38 -0300

    test(coupons): Coupons validation unit test

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe "Coupon" do
+
+  context "validation" do
+
+    it "code must be unique" do
+      # Arrange
+      promotion = Promotion.create!(
+        name: 'Black Friday',
+        description: 'Super Black Friday',
+        code: 'BLACK50',
+        discount_rate: 50,
+        coupon_quantity: 100,
+        expiration_date: Time.now.strftime('%d/%m/%Y')
+      )
+
+      Coupon.create!(
+        code: 'BLACK50-0002',
+        promotion_id: promotion.id
+      )
+
+      #  Act
+      coupon = Coupon.create!(
+        code: 'BLACK50-0002',
+        promotion_id: promotion.id
+      )
+
+      coupon.valid?
+      
+      # Assert
+      expect(coupon.errors[:code]).to include('must be unique')
+    
+    end
+  end
+end
