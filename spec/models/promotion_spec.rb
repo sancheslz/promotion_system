be65236d92ff7a3c40bf107b6f3c3030a8c77211
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-06 22:08:47 -0300

    test(promotion): Promotion validation unit test

--- spec/models/promotion_spec.rb
+++ spec/models/promotion_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
-RSpec.describe Promotion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Promotion" do
+
+  context "validation" do
+
+    it "fields can't be blank" do
+      # Arrange
+      promotion = Promotion.new
+
+      # Act
+      promotion.valid?
+
+      # Assert
+      expect(promotion.errors[:name]).to include("can't be blank") 
+      expect(promotion.errors[:description]).not_to include("can't be blank") 
+      expect(promotion.errors[:code]).to include("can't be blank") 
+      expect(promotion.errors[:discount_rate]).to include("can't be blank") 
+      expect(promotion.errors[:coupon_quantity]).to include("can't be blank") 
+      expect(promotion.errors[:expiration_date]).to include("can't be blank") 
+
+    end
+
+    it "code must be unique" do
+      # Arrange 
+      Promotion.create!(
+        name: 'Black Friday',
+        description: '',
+        code: 'BLACK50',
+        discount_rate: 50,
+        coupon_quantity: 100,
+        expiration_date: Time.now.strftime('%d/%m/%Y')
+      )
+
+      # Act
+      promotion = Promotion.new(
+        name: 'Thanks Given',
+        description: '',
+        code: 'BLACK50',
+        discount_rate: 50,
+        coupon_quantity: 100,
+        expiration_date: Time.now.strftime('%d/%m/%Y')
+      )
+      promotion.valid?
+
+      # Assert
+      expect(promotion.errors[:code]).to include('must be unique')
+      
+    end
+    
+  end
+  
 end
