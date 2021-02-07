24a49cdfb5d85e3068eccfd435035477031a5d59
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-07 12:52:48 -0300

    test(promotions): Admin can delete a registered promotion

--- spec/features/admin_manage_promotions_spec.rb
+++ spec/features/admin_manage_promotions_spec.rb
@@ -339,3 +339,93 @@ feature('Admin edits a promotion') do
     end
 
 end
+
+feature('Admin deletes a promotion') do
+    
+    scenario('can do it') do
+        # Arrange
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super promotion of Black Friday',
+            code: 'BLACK50',
+            discount_rate: '50',
+            coupon_quantity: '100',
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+        )
+
+        # Act
+        visit root_path
+        click_on 'Promotions'
+        click_on promotion.name
+
+        # Assert
+        expect(page).to have_link('Delete Promotion')
+    end
+
+    scenario('have an alert message') do
+        # Arrange
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super promotion of Black Friday',
+            code: 'BLACK50',
+            discount_rate: '50',
+            coupon_quantity: '100',
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+        )
+
+        # Act
+        visit root_path
+        click_on 'Promotions'
+        click_on promotion.name
+        click_on 'Delete Promotion'
+
+        # Assert
+        expect(page).to have_content('Do you really want to delete this promotion?')
+    end
+
+    scenario('delete successfully') do
+        # Arrange
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super promotion of Black Friday',
+            code: 'BLACK50',
+            discount_rate: '50',
+            coupon_quantity: '100',
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+        )
+
+        # Act
+        visit root_path
+        click_on 'Promotions'
+        click_on promotion.name
+        click_on 'Delete Promotion'
+        click_on 'Confirm'
+
+        # Assert
+        expect(current_path).to eq(promotions_path)
+        expect(page).not_to have_content(promotion.name)
+    end
+
+    scenario('can cancel the operation') do
+        # Arrange
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super promotion of Black Friday',
+            code: 'BLACK50',
+            discount_rate: '50',
+            coupon_quantity: '100',
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+        )
+
+        # Act
+        visit root_path
+        click_on 'Promotions'
+        click_on promotion.name
+        click_on 'Delete Promotion'
+        click_on 'Cancel'
+
+        # Assert
+        expect(current_path).to eq(promotion_path(promotion))
+    end
+
+end
