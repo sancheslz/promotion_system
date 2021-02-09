8b3b703ac47d105e89fe00ba189e4c364859090b
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-09 01:32:01 -0300

    test(categories): Categories validation unit test

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "Category" do
+  
+  context "validation" do
+
+    it "fiels can't be blank" do
+      # Arrange
+      category = Category.new
+
+      # Act
+      category.valid?
+
+      # Assert
+      expect(category.errors[:name]).to include('can\'t be blank')
+      expect(category.errors[:code]).to include('can\'t be blank')
+    end
+
+    it "code must be unique" do
+      # Arrange
+      Category.create!(
+        name: 'Electronics',
+        code: 'ELEKTRO'
+      )
+
+      # Act
+      category = Category.new(
+        name: 'Computers',
+        code: 'ELEKTRO'
+      )
+      category.valid?
+
+      # Assert
+      expect(category.errors[:code]).to include('must be unique')
+    end
+    
+  end
+  
 end
