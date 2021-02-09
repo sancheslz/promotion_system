6b3618f19ce4bda57e6d8e96d73d62eeeecf692c
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-09 15:28:12 -0300

    test(promotion_approval): Only generates coupons of approved promotions

--- spec/features/approval_promotions_spec.rb
+++ spec/features/approval_promotions_spec.rb
@@ -135,3 +135,74 @@ feature('Creator can\'t approve') do
     end
 
 end
+
+feature('Only approved promotions can generate coupons') do
+    
+    scenario('approved promotion') do
+        # Arrange 
+        creator = User.create!(
+            email: 'creator@host.com',
+            password: 'u1s2e3r'
+        )
+
+        approver = User.create(
+            email: 'approver@host.com',
+            password: 'u1s2e3r'
+        )
+
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super Black Friday',
+            code: 'BLACK50',
+            discount_rate: 50,
+            coupon_quantity: 100,
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+            user: creator
+        )
+
+        # Act
+        login_as approver, :scope => :user 
+        visit root_path 
+        click_on 'Promotions'
+        click_on promotion.name 
+        click_on 'Approve Promotion'
+        click_on 'Generate Coupons'
+
+        # Assert
+        expect(page).to have_content('BLACK50-', count: promotion.coupon_quantity)
+    end
+
+    scenario('unapproved promotion') do
+        # Arrange 
+        creator = User.create!(
+            email: 'creator@host.com',
+            password: 'u1s2e3r'
+        )
+
+        approver = User.create(
+            email: 'approver@host.com',
+            password: 'u1s2e3r'
+        )
+
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super Black Friday',
+            code: 'BLACK50',
+            discount_rate: 50,
+            coupon_quantity: 100,
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+            user: creator
+        )
+
+        # Act
+        login_as approver, :scope => :user 
+        visit root_path 
+        click_on 'Promotions'
+        click_on promotion.name
+
+        # Assert
+        expect(page).not_to have_link('Generate Coupons') 
+        expect(page).not_to have_content('BLACK50-', count: promotion.coupon_quantity)
+    end
+
+end
