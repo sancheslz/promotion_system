5506c353c0fe01d0987dbb3ddc2054b733fe2f6d
Lucas Sanches <sanches.lz@hotmail.com>
2021-02-09 16:26:14 -0300

    test(promotions_approvel): Display who approved and when

--- spec/features/approval_promotions_spec.rb
+++ spec/features/approval_promotions_spec.rb
@@ -206,3 +206,78 @@ feature('Only approved promotions can generate coupons') do
     end
 
 end
+
+
+feature('Display who approved') do
+
+    scenario('on promotion detail') do
+        # Arrange
+         # Arrange 
+         creator = User.create!(
+            email: 'creator@host.com',
+            password: 'u1s2e3r'
+        )
+
+        approver = User.create(
+            email: 'approver@host.com',
+            password: 'u1s2e3r'
+        )
+
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super Black Friday',
+            code: 'BLACK50',
+            discount_rate: 50,
+            coupon_quantity: 100,
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+            user: creator,
+        )
+
+        # Act
+        login_as approver, :scope => :user
+        visit root_path 
+        click_on 'Promotions'
+        click_on 'Approve Promotion'
+        promotion.reload
+
+        # Assert
+        expect(page).to have_content("Approved by: #{promotion.approved_by}")
+        expect(page).to have_content("Approved at: #{promotion.approved_at}")
+    end
+
+    scenario('or nothing') do
+        # Arrange
+         # Arrange 
+         creator = User.create!(
+            email: 'creator@host.com',
+            password: 'u1s2e3r'
+        )
+
+        approver = User.create(
+            email: 'approver@host.com',
+            password: 'u1s2e3r'
+        )
+
+        promotion = Promotion.create!(
+            name: 'Black Friday',
+            description: 'Super Black Friday',
+            code: 'BLACK50',
+            discount_rate: 50,
+            coupon_quantity: 100,
+            expiration_date: Time.now.strftime('%d/%m/%Y'),
+            user: creator,
+        )
+
+        # Act
+        login_as approver, :scope => :user
+        visit root_path 
+        click_on 'Promotions'
+        click_on 'Approve Promotion'
+        promotion.reload
+
+        # Assert
+        expect(page).not_to have_content("Approved by: #{promotion.approved_by}")
+        expect(page).not_to have_content("Approved at: #{promotion.approved_at}")
+    end
+
+end
